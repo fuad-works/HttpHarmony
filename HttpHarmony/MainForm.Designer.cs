6df1b5361b58873c58e36731979f91a64c07957b
Fuad Mehawish <fuad.mehawish@gmail.com>
2024-02-29 15:34:03 +0300

    Start Addin controlls and Tabs functionaliyu

--- HttpHarmony/MainForm.Designer.cs
+++ HttpHarmony/MainForm.Designer.cs
@@ -28,19 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            treeView1 = new TreeView();
+            tabControl1 = new TabControl();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // Form1
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.Left;
+            toolStrip1.GripStyle = ToolStripGripStyle.Hidden;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(24, 510);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(21, 20);
+            toolStripButton1.Text = "toolStripButton1";
+            // 
+            // treeView1
+            // 
+            treeView1.Dock = DockStyle.Left;
+            treeView1.Location = new Point(24, 0);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(157, 510);
+            treeView1.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(181, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(637, 510);
+            tabControl1.TabIndex = 2;
+            // 
+            // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
+            ClientSize = new Size(818, 510);
+            Controls.Add(tabControl1);
+            Controls.Add(treeView1);
+            Controls.Add(toolStrip1);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Main Form";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private TreeView treeView1;
+        private TabControl tabControl1;
     }
 }
---
+++ HttpHarmony/WorkTabItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HttpHarmony
+{
+    public partial class WorkTabItem : UserControl
+    {
+        public WorkTabItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
